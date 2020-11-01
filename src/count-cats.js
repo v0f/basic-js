ccc1fda02291f7a220c0c8e61a4cd765ca064ea4
vof <vof1111@gmail.com>
2020-11-01 23:53:37 +0300

    feat: implement tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(cats) {
+  return cats.flat().reduce((acc,x)=>String(x)=='^^' ? acc + 1 : acc, 0);
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date=null) {
+  if(date===null)
+    return 'Unable to determine the time of year!';
+  if(Object.prototype.toString.call(date) != '[object Date]')
+    throw Error;
+
+    let seasons = ['winter','spring','summer','autumn'];
+    seasons = seasons.map(x=>`${x} `.repeat(3)).join('').trimRight().split(' ');
+    seasons.push(seasons.shift());
+    try {
+      return seasons[date.getMonth()]
+    } catch (error) {
+      throw Error;
+    }
+
 };
