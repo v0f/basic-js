9c46af3a53a6f5836729fc84d367892d13474353
vof <vof1111@gmail.com>
2020-11-01 23:59:04 +0300

    feat: implement tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,10 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity ) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (typeof (sampleActivity) === 'string'
+    && Number(sampleActivity) > 0
+    && Number(sampleActivity) <= MODERN_ACTIVITY) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity) * (HALF_LIFE_PERIOD / Math.log(2))));
+  }
+  return false;
 };
